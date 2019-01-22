be4a1d1d7446b968d55b35d10823609800bfebdc
ensenen <kachun51@gmail.com>
2019-01-22 13:18:38 +0800

    Update clock_upper_case.py

--- clock_upper_case.py
+++ clock_upper_case.py
@@ -1,1 +1,11 @@
+from hanover_flipdot import display
+from hanover_flipdot import fonts
+import time
 
+display = display.Display("/dev/tty.wchusbserial1a1230", 5, 144, 19, fonts.unscii_mcr, True, False)
+
+while True:
+    display.write_text(time.strftime("%H:%M:%S").upper(), line = 3, column = 40)
+    display.write_text(time.strftime("%a %d %b %Y").upper(), line = 11, column = 8)
+    display.send()
+    time.sleep(.3)
